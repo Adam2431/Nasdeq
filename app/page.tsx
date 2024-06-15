541597e21b511efe65f22cb249986e3378dcdbf3
Adam Samy <83244653+Adam2431@users.noreply.github.com>
2024-06-15 20:39:47 +0300

    Added splash screen and added loading animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,40 @@
+"use client";
 import Image from "next/image";
-import { getStocks } from "./actions/getStocks";
 import StockBox from "./StockBox";
+import { useState } from "react";
 
 export default async function Home() {
+  const [splash, setSplash] = useState(true);
+
   return (
-    <main className="dark flex min-h-screen flex-col items-center justify-between bg-[#1f202f] min-w-full">
-      <div className="min-w-full">
+    <main
+      className="dark flex min-h-screen flex-col items-center justify-between bg-[#1f202f] min-w-full"
+      onClick={() => {
+        setSplash(false);
+      }}
+    >
+      <div className={`${splash ? "block" : "hidden"}`}>
+        <div className="w-full">
+          <div className="flex flex-col h-screen items-center justify-center">
+            <Image
+              src="/nasdeq.png"
+              alt="Nasdeq"
+              width={300}
+              height={300}
+              className="mt-auto"
+            />
+            <div className="w-full justify-center items-center text-center text-lg text-slate-500">
+              Tap to continue
+            </div>
+            <div className="w-full text-center text-2xl mt-auto mb-5">Adam Samy</div>
+          </div>
+        </div>
+      </div>
+      <div className={`min-w-full ${splash ? "hidden" : "block"}`}>
         <nav className="flex items-center justify-between w-full px-5 pb-1 pt-10 bg-[#191a28]">
           <Image
             src="/nasdeq.png"
-            alt="Nasdeq"
+            alt="Nasdaq"
             width={125}
             height={125}
             className="mb-4"
@@ -18,7 +43,7 @@ export default async function Home() {
         <div className="p-5">
           <div>
             <h1 className="text-3xl font-bold text-white text-center">
-              Welcome to Nasdeq
+              Welcome to Nasdaq
             </h1>
             <p className="text-lg text-gray-400 text-center">
               The best place to find stocks
